7f3946ebe036502eaa720c0f5a7f3888233e3101
ben <biovino@gmail.com>
2023-10-25 11:57:15 -0400

    added microbenchmark results

---
+++ benchmark_llr.R
@@ -0,0 +1,11 @@
+install.packages("microbenchmark")
+library(microbenchmark)
+source("llr_functions.R")
+
+n = 15
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+
+benchmark_result <- microbenchmark(llr(x, y, z, 1))
+print(benchmark_result)
